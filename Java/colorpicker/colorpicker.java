316ceb78443286a5035bf75ad24e7c2d03e97687
WarGravy <zackcarlson13@gmail.com>
2014-12-03 00:32:15 -0800

    Color picker updates

--- Java/colorpicker/colorpicker.java
+++ Java/colorpicker/colorpicker.java
@@ -6,14 +6,21 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
+import java.util.Vector;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 @SuppressWarnings("unchecked")
 public class ColorPicker extends JFrame{
 	
-	protected JButton buttonDone, buttonCancel;
-	protected JTextField tfFirstName, tfLastName;
+	protected ColorPanel panelColor;
+	protected JLabel labelR, labelG, labelB;
+	protected JButton save, reset, rMinus, rPlus, gMinus, gPlus, bMinus, bPlus;
+	protected JTextField tfRed, tfGreen, tfBlue;
 	protected JList listColors;
-
+	protected Graphics graphics;
+	protected Vector<ColorSample> pallete;
 	public static void main(String[] args) {
 		new ColorPicker("Color Picker Application");
 	}
@@ -21,35 +28,117 @@ public class ColorPicker extends JFrame{
 	public ColorPicker(String title) 
 	{
 		super(title);		// call constructor of base class
-		setSize(500, 200);
-		//setBounds(100, 100, 250, 100);
-
+		setSize(430, 450);
 		addWindowListener(new WindowDestroyer());
 
+		//initialize variables
+		pallete = new Vector<ColorSample>();
+		//lists
 		listColors = new JList();
 		listColors.addListSelectionListener(new ListHandler());
+		//buttons
+		save = new JButton("Save");
+		reset = new JButton("Reset");
+		rMinus = new JButton("-");
+		rPlus = new JButton("+");
+		gMinus = new JButton("-");
+		gPlus = new JButton("+");
+		bMinus = new JButton("-");
+		bPlus = new JButton("+");
+		save.addActionListener(new ActionHandler());  
+		reset.addActionListener(new ActionHandler());
+		rMinus.addActionListener(new ActionHandler());  
+		rPlus.addActionListener(new ActionHandler());
+		gMinus.addActionListener(new ActionHandler());  
+		gPlus.addActionListener(new ActionHandler());
+		bMinus.addActionListener(new ActionHandler());  
+		bPlus.addActionListener(new ActionHandler());
+		//textfields
+		tfRed = new JTextField("");
+		tfGreen = new JTextField("");
+		tfBlue = new JTextField("");
+		//color panel
+		panelColor = new ColorPanel();
+		//labels
+		labelR = new JLabel("Red:");
+		labelG = new JLabel("Green:");
+		labelB = new JLabel("Blue:");
 
-		buttonDone = new JButton("Done");
-		buttonCancel = new JButton("Cancel");
-		buttonDone.addActionListener(new ActionHandler());  
-		buttonCancel.addActionListener(new ActionHandler());
+		//layout
+		getContentPane().setLayout(null);
+		
+		//color panel
+		getContentPane().add(panelColor);
+		panelColor.setBounds(10, 10, 280, 200);
+		//list pane
+		JScrollPane jscrollpane = new JScrollPane(listColors);
+		getContentPane().add(jscrollpane);
+		jscrollpane.setBounds(300, 10, 100, 380);
+		//red row
+		getContentPane().add(labelR);
+		labelR.setBounds(30, 220, 50, 30);
+		getContentPane().add(tfRed);
+		tfRed.setBounds(90, 220, 50, 30);
+		getContentPane().add(rMinus);
+		rMinus.setBounds(150, 220, 50, 30);
+		getContentPane().add(rPlus);
+		rPlus.setBounds(210, 220, 50, 30);
+		//green row
+		getContentPane().add(labelG);
+		labelG.setBounds(30, 260, 50, 30);
+		getContentPane().add(tfGreen);
+		tfGreen.setBounds(90, 260, 50, 30);
+		getContentPane().add(gMinus);
+		gMinus.setBounds(150, 260, 50, 30);
+		getContentPane().add(gPlus);
+		gPlus.setBounds(210, 260, 50, 30);
+		//blue row
+		getContentPane().add(labelB);
+		labelB.setBounds(30, 300, 50, 30);
+		getContentPane().add(tfBlue);
+		tfBlue.setBounds(90, 300, 50, 30);
+		getContentPane().add(bMinus);
+		bMinus.setBounds(150, 300, 50, 30);
+		getContentPane().add(bPlus);
+		bPlus.setBounds(210, 300, 50, 30);
+		//save and reset
+		getContentPane().add(save);
+		save.setBounds(50, 350, 70, 30);
+		getContentPane().add(reset);
+		reset.setBounds(150, 350, 70, 30);
 
-		tfFirstName = new JTextField("");
-		tfLastName = new JTextField("");
+		//read file and build list of ColorSamples
+		File file = new File("save.txt");
 
-		getContentPane().setLayout(new GridLayout(5, 2));
-		getContentPane().add(new JLabel("First Name:"));
-		getContentPane().add(tfFirstName);
-		getContentPane().add(new JLabel("Last Name:"));
-		getContentPane().add(tfLastName);
-		getContentPane().add(buttonDone);
-		getContentPane().add(buttonCancel);
-		getContentPane().add(new JScrollPane(listColors));
+    	try {
 
-		String colors[] = {"Red", "Green", "Blue", "Yellow", "Cyan", "Magenta", "Orange", "Pink", "Grey", "Black", "White"};
+	        Scanner sc = new Scanner(file);
+
+	        while (sc.hasNextLine()) {
+	            String temp = sc.next();
+	            int r = sc.nextInt();
+	            int g = sc.nextInt();
+	            int b = sc.nextInt();
+	            System.out.println(temp);
+	            System.out.println(r);
+	            System.out.println(g);
+	            System.out.println(b);
+	            pallete.add(new ColorSample(temp,r,g,b));
+        	}
+        	sc.close();
+    	} 
+		catch (FileNotFoundException e) {
+        e.printStackTrace();
+    	}
+		//build vector of colors
+		Vector<String> colors = new Vector<String>();
+		//set list data
 		listColors.setListData(colors);
-        tfFirstName.setText("John");
-        tfLastName.setText("John");
+
+		//set data for RGB
+        tfRed.setText("255");
+        tfGreen.setText("255");
+        tfBlue.setText("255");
 
 		setVisible(true);
 	}
@@ -68,14 +157,41 @@ public class ColorPicker extends JFrame{
 	{      
 		public void actionPerformed(ActionEvent e)
 		{
-			if ( e.getSource() == buttonDone )
+			if ( e.getSource() == save )
 			{
-				String s1 = tfFirstName.getText();
-				String s2 = tfLastName.getText();
-				System.out.println("Full name: " + s1 + " " + s2);
+				String s1 = tfRed.getText();
+				String s2 = tfGreen.getText();
+				String s3 = tfBlue.getText();
+				System.out.println("Full name: " + s1 + " " + s2 + " " + s3);
 			}
-			else if ( e.getSource() == buttonCancel )
-				System.out.println("You pressed the Cancel button.");
+			else if ( e.getSource() == reset ){
+				System.out.println("You pressed the Reset button.");
+
+			}
+			else if(e.getSource() == rMinus){
+				tfRed.setText(ColorCounter(tfRed.getText(), false));
+
+			}
+			else if(e.getSource() == rPlus){
+				tfRed.setText(ColorCounter(tfRed.getText(), true));
+				
+			}
+			else if(e.getSource() == gMinus){
+				tfGreen.setText(ColorCounter(tfGreen.getText(), false));
+				
+			}
+			else if(e.getSource() == gPlus){
+				tfGreen.setText(ColorCounter(tfGreen.getText(), true));
+				
+			}
+			else if(e.getSource() == bMinus){
+				tfBlue.setText(ColorCounter(tfBlue.getText(), false));
+				
+			}
+			else if(e.getSource() == bPlus){
+				tfBlue.setText(ColorCounter(tfBlue.getText(), true));
+			}
+			
 		}
 	} 
 	// Define list listener                                       
@@ -92,5 +208,45 @@ public class ColorPicker extends JFrame{
 		 
 		}
 	}
-                       
+    //String to Int to String Color converter
+    public String ColorCounter(String s, boolean addition){
+    	int i = Integer.parseInt(s);
+    	if(addition && i < 255){
+    		i+=5;
+    	}
+    	else if(!addition && i > 0){
+    		i-=5;
+    	}
+    	if(i > 255)
+    		i = 255;
+    	if(i < 0)
+    		i = 0;
+    	panelColor.repaint();
+    	return String.valueOf(i);
+    } 
+    class ColorPanel extends JComponent
+	{
+		public void paint(Graphics g)
+		{
+			Dimension d = getSize();
+
+			g.setColor(new Color(Integer.parseInt(tfRed.getText()),Integer.parseInt(tfGreen.getText()),Integer.parseInt(tfBlue.getText())));
+			g.fillRect(1, 1, d.width-2, d.height-2);
+		
+		}
+	}
+    class ColorSample
+	{
+		public int red;
+		public int green;
+		public int blue;
+		public String name;
+
+		public ColorSample(String title, int r, int g, int b){
+			red = r;
+			green = g;
+			blue = b;
+			name = title;
+		}
+ 	}             
 }
