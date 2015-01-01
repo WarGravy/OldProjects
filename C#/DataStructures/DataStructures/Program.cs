b5b6ea92b0e83d5a4eada34feaacc589b685de78
WarGravy <zackcarlson13@gmail.com>
2015-01-01 14:34:33 -0800

    Initial commit of Data Structures in C#

---
+++ C#/DataStructures/DataStructures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * Data Structures in C#
+ * Written by Zachary Ty Carlson
+ * Last Edited: 1/1/15
+ */
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
